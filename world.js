3ea1fbe86014b93480369b504a2c93b6f31c92de
adamleemorris <57811531+adamleemorris@users.noreply.github.com>
2023-11-19 16:58:16 -0500

    Add files via upload

---
+++ world.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("lookup").addEventListener("click", function () {
+    var country = encodeURIComponent(document.getElementById("country").value);
+    var url = "world.php?country=" + country;
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json(); // Parse JSON response here
+      })
+      .then((data) => {
+        document.getElementById("result").innerHTML = data;
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  });
+});
--- world.php
+++ world.php
@@ -1,17 +1,42 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Check if 'country' parameter is set
+if(isset($_GET['country'])) {
+    // Sanitize the input to prevent SQL injection
+    $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
+    
+    // Fetch data for the specified country
+    $stmt1 = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+    $results = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+    
+     // Generate HTML table
+     $html_results = '<table>';
+     $html_results .= '<tr>';
+     $html_results .= '<th>Country Name</th>';
+     $html_results .= '<th>Continent</th>';
+     $html_results .= '<th>Independence Year</th>';
+     $html_results .= '<th>Head of State</th>';
+     $html_results .= '</tr>';
+     
+     foreach ($results as $row) {
+         $html_results .= '<tr>';
+         $html_results .= '<td>' . $row['name'] . '</td>';
+         $html_results .= '<td>' . $row['continent'] . '</td>';
+         $html_results .= '<td>' . $row['independence_year'] . '</td>';
+         $html_results .= '<td>' . $row['head_of_state'] . '</td>';
+         $html_results .= '</tr>';
+     }
+ 
+     $html_results .= '</table>';
 
+    // Output JSON response and stop further execution
+    echo json_encode($html_results);
+    exit;
+}
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>

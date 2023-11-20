c071f6be740cbdf1e138d2f316c6f5bc49e42db2
adamleemorris <57811531+adamleemorris@users.noreply.github.com>
2023-11-20 02:30:47 -0500

    Add files via upload

--- world.js
+++ world.js
@@ -17,4 +17,23 @@ document.addEventListener("DOMContentLoaded", function () {
         console.error("There was a problem with the fetch operation:", error);
       });
   });
+
+  document.getElementById("lookup_city").addEventListener("click", function () {
+    var country = encodeURIComponent(document.getElementById("country").value);
+    var url = "world.php?country=" + country + "&lookup=cities";
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json(); // Parse JSON response here
+      })
+      .then((data) => {
+        document.getElementById("result").innerHTML = data;
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  });
 });
